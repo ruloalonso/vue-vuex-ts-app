845a928f64a4a2a0e6c2e2239881fdbc18fd8c36
Raul <raul.alonso@servexternos.repsol.com>
2018-11-29 11:51:18 +0100

    todo view

---
+++ src/views/TodoApp/component.ts
@@ -0,0 +1,23 @@
+import { Component, Vue } from "vue-property-decorator";
+import TodoForm from '@/components/TodoForm/component';
+import TodoList from '@/components/TodoList/component';
+import { Action, Getter, State } from 'vuex-class';
+import { Todo } from '@/store/modules/todo/types';
+const namespace: string = 'todoModule';
+
+@Component({
+  components: {
+    TodoList, TodoForm
+  }
+})
+export default class TodoApp extends Vue {
+  @Action('fetchData', {namespace}) fetchData!: Function;
+  @Getter('todos', {namespace}) todos!: Todo[];
+  @Getter('done', {namespace}) done!: Todo[];
+  @State('error', {namespace}) errorLoadingTodos!: boolean;
+  @State('errorMessage', {namespace}) errorMessage!: string;
+
+  mounted() {
+    this.fetchData();
+  }
+}
---
+++ src/views/TodoApp/template.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div v-if="!errorLoadingTodos">
+      <b-row class="my-5">
+        <todo-form class="mx-auto"/>
+      </b-row>
+      <b-row>
+        <b-col>
+          <h2 class="text-center text-muted">Todos pending</h2>
+          <todo-list :todos="todos"/>
+        </b-col>
+
+        <b-col>
+          <h2 class="text-center text-muted">Todos finished</h2>
+          <todo-list :todos="done"/>
+        </b-col>
+      </b-row>
+    </div>
+    <div v-else class="alert alert-danger text-center">
+      Error cargando los todos
+      <p>{{ errorMessage }}</p>
+    </div>
+  </div>
+</template>
